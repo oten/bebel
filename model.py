7f9ed29103157d5d3a6f4b4b2e051215b8ff70cc
Antonio <antoniomrneto@gmail.com>
2015-08-17 22:29:34 -0300

    Add simple model for Tags

--- model.py
+++ model.py
@@ -2,9 +2,9 @@ import db
 
     
 class Codes(db.TransversalModel):
-    code = db.Table('code', path='./db')
-    language = db.Table('language', path='./db')
-    tags = db.Table('tags', path='./db')
+    code = db.Table('codes_code', path='./db')
+    language = db.Table('codes_language', path='./db')
+    tags = db.Table('codes_tags', path='./db')
     codes_of_tag = db.Mirror(tags)
     
     def __init__(self):
@@ -16,3 +16,8 @@ class Codes(db.TransversalModel):
     def add(self, **kwarg):
         super(Codes, self).add(str(self.next_idx), **kwarg)
         self.next_idx += 1
+
+
+class Tags(db.Table):
+    def __init__(self):
+        super(Tags, self).__init__('tags', path='./db')
